8dda2688b1c6244c5aa92e79e00197746e43ef73
Mahesh <maheshgowda3501@gmail.com>
2026-02-01 18:23:31 +0530

    Added .env to .gitignore and saved API_KEY inside the .env file

---
+++ video_stats.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+
+API_KEY = os.getenv("API_KEY") 
+
+#API_KEY = "AIzaSyCj0C-oXQchklhJ_oZ6_EmhctTTkuHzrzw"
+#CHANNEL_HANDLE = "MrBeast"
+
+def get_playlist_id():
+
+    try:
+        
+        url = "https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle=MrBeast&key=AIzaSyCj0C-oXQchklhJ_oZ6_EmhctTTkuHzrzw"
+
+        response = requests.get(url)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        #print(json.dumps(data, indent=4))
+
+        #data.items[0].contentDetails.relatedPlaylists.uploads
+
+        channel_items = data['items'][0]
+
+        channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
+
+        print(channel_playlistId)
+
+        return channel_playlistId
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+if __name__ == "__main__":
+    #print("get_playlist_id will be executed")
+    get_playlist_id()
+#else:
+    #print("get_playlist_id will not be executed")
